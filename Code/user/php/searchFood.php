a124507fea5605496e5eff014bc30ca179e0280a
hafyze <84822836+hafyze@users.noreply.github.com>
2023-06-06 22:30:31 +0800

    Update Search Feature

---
+++ Code/user/php/searchFood.php
@@ -0,0 +1,33 @@
+<?php
+
+include("../php/dataconnection.php");
+
+// Check if the search term is provided in the query parameters
+if (isset($_GET['term'])) {
+    $searchTerm = $_GET['term'];
+
+    // Perform your search logic here
+    $sql = "SELECT * FROM Food WHERE name LIKE '%searchTerm'";
+
+    $result = $connection->query($sql);
+
+    if ($result) {
+        // Display the search results
+        if ($result->num_rows > 0) {
+            echo "<ul>";
+            while ($row = $result->fetch_assoc()) {
+                // Access individual rows and columns
+                $foodName = $row['name'];
+                // Generate HTML content for each search result
+                echo "<li>$foodName</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "No results found.";
+        }
+    } 
+}
+
+// Close the database connection
+$conn->close();
+?>
