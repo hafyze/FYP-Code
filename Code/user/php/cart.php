020048d096dd5f5382f1ddeb35e35fedd0ee61c8
hafyze <84822836+hafyze@users.noreply.github.com>
2023-06-12 22:34:56 +0800

    Cart feature done, proceed with payment next

--- Code/user/php/cart.php
+++ Code/user/php/cart.php
@@ -17,25 +17,31 @@ session_start();
 
 if (isset($_SESSION['cart'])) {
     $cartItems = $_SESSION['cart'];
-
+    echo "<h2>Item Cart</h2>";
     if (!empty($cartItems)) {
+
+        echo "<form action='../php/updateCart.php' method='POST'>";
+
         foreach ($cartItems as $key => $item) {
+
             if (is_array($item) && isset($item['product'])) {
                 $productName = $item['product'];
                 $quantity = $item['quantity'];
         
-                echo "<li>$productName (Quantity: ";
+                echo "<li>$productName ";
                 echo "<input type='number' name='quantity[$key]' value='$quantity' min='1'>";
-                echo ")</li>";
+                echo "</li>";
                 echo "<a href='../php/deleteCart.php?index=$key'>Delete</a>";
                 echo "</li>";
             }
         }
         echo "<input type='submit' value='Update Quantity'>";
         echo "</form>";
+
     } else {
         echo "<li>Cart is empty</li>";
     }
+
 } else {
     echo "<li>Cart is empty</li>";
 }
