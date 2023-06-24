8c3c5ddc0ef3715574d9e1bb01db4387e11e1ff8
hafyze <hafizmatqwer@gmail.com>
2023-06-24 19:52:13 +0800

    Register:Check existing email and password confirmation

--- Code/user/php/register.php
+++ Code/user/php/register.php
@@ -3,21 +3,39 @@ session_start();
 
 include("../php/dataconnection.php");
 
+
+$error_message = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Data from form is declared 
     $customer_name = $_POST["customer_name"];
     $customer_contact = $_POST["customer_contact"];
     $customer_email = $_POST["customer_email"];
     $customer_pass = $_POST["customer_pass"];
+    $confirm_pass = $_POST["confirm_password"];
 
-    // Insert user into the database
-    $sql = "INSERT INTO customer (customer_name, customer_contact, customer_email, customer_pass) 
-            VALUES ('$customer_name', '$customer_contact', '$customer_email', $customer_pass)";
-    
-    if ($connection->query($sql) === TRUE) {
-        header("Location: ../html/index.html");
-    } else {
-        echo "Error: " . $sql . "<br>";
+    // password comparison
+    if ($customer_pass !== $confirm_pass) {
+        $error_message = "Error: Passwords do not match.";
+        echo "<script>alert('$error_message');</script>"; // Error popup
+    } 
+    else {
+        // check for existing email
+        $email_check_query = "SELECT * FROM customer WHERE customer_email='$customer_email' LIMIT 1";
+        $email_result = $connection->query($email_check_query);
+        if ($email_result->num_rows > 0) {
+            $error_message = "Error: Email already registered.";
+            echo "<script>alert('$error_message');</script>"; // Error popup
+        } 
+        else {
+            $sql = "INSERT INTO customer (customer_name, customer_contact, customer_email, customer_pass) 
+                    VALUES ('$customer_name', '$customer_contact', '$customer_email', '$customer_pass')";
+
+            if ($connection->query($sql) === TRUE) {
+                header("Location: ../html/index.html");
+            } else {
+                echo "Error: " . $sql . "<br>";
+            }
+        }
     }
 }
 
