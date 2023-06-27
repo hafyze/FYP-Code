e5683acb9dfc9219fda23dc61cbe3ce5e7e229ae
hafyze <hafizmatqwer@gmail.com>
2023-06-28 03:55:12 +0800

    Fix register where it puts 2 data in db, Reset Password complete

--- Code/user/php/register.php
+++ Code/user/php/register.php
@@ -3,7 +3,6 @@ session_start();
 
 include("../php/dataconnection.php");
 
-
 $error_message = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -13,41 +12,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $customer_pass = $_POST["customer_pass"];
     $confirm_pass = $_POST["confirm_password"];
 
-    // password comparison
+    // Password comparison
     if ($customer_pass !== $confirm_pass) {
         $error_message = "Error: Passwords do not match.";
         echo "<script>alert('$error_message');</script>"; // Error popup
-    } 
-    else {
-        // check for existing email
+    } else {
+        // Check for existing email
         $email_check_query = "SELECT * FROM customer WHERE customer_email='$customer_email' LIMIT 1";
         $email_result = $connection->query($email_check_query);
         if ($email_result->num_rows > 0) {
             $error_message = "Error: Email already registered.";
             echo "<script>alert('$error_message');</script>"; // Error popup
-        } 
-        else {
+        } else {
             $sql = "INSERT INTO customer (customer_name, customer_contact, customer_email, customer_pass) 
                     VALUES ('$customer_name', '$customer_contact', '$customer_email', '$customer_pass')";
 
             if ($connection->query($sql) === TRUE) {
                 header("Location: ../html/index.html");
+                exit; // Exit after redirection
             } else {
                 echo "Error: " . $sql . "<br>";
             }
         }
     }
 }
-    // Insert user into the database
-    $sql = "INSERT INTO customer (customer_name, customer_contact, customer_email, customer_pass) 
-        VALUES ('$customer_name', '$customer_contact', '$customer_email', '$customer_pass')";
-
-    if ($connection->query($sql) === TRUE) {
-        header("Location: ../html/index.html");
-    } else {
-        echo "Error: " . $sql . "<br>";
-}
-
 
 $connection->close();
 ?>
