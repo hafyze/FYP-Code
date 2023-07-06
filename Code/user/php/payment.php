6cd3f185f4cce9a87ae899f77260e7d1718bab72
hafyze <hafizmatqwer@gmail.com>
2023-07-06 11:14:26 +0800

    Update

--- Code/user/php/payment.php
+++ Code/user/php/payment.php
@@ -10,15 +10,82 @@
 
     <script src="https://kit.fontawesome.com/fd65af94cc.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
     <link rel="stylesheet" href="../css/style.css" <?php echo time(); ?>>
     <link rel="stylesheet" href="../css/payment.css" <?php echo time(); ?>>
     <script src="../js/index.js"></script>
 
 </head>
 
+<style>
+        body{
+            background-color: #ffffff;
+            background-image: linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%);       
+        }
+        .payment-form .form-control {
+            width: 100%;
+            max-width: 300px; 
+            margin: 5px auto;
+        }
+
+        h3{
+            text-align: center;
+        }
+
+        header{
+            border-radius: 20px;
+        }
+
+        #navbar ul li a {
+            display: block;
+            padding: 10px 15px;
+            text-decoration: none;
+            color: white;
+            font-weight: bold;
+            border-radius: 8%;
+            transition: all 0.3s ease;
+        }
+
+        #navbar ul li a:hover {
+            color: #333;
+            background-color: #f5f5f5;
+        }
+
+        #tdvText{
+            width: fit-content;
+            margin: 0 auto;
+            padding: 15px;
+            background-color: #657566;
+            border-radius: 15px;
+            text-decoration: none;
+        }
+
+        #tdvText a{
+            color: white;
+        }
+
+        #tdvText:hover{
+            background-color: #333;
+            transition-duration: 1.3s;
+        }
+
+        #tdvText a:hover{
+            text-decoration: none;
+        }
+
+        .table-rounded{
+            border-radius: 10px;
+        }
+
+        .table td{
+            background-color: white;
+        }
+    </style>
+
 <body>
-    <h1 class="restText"><a href="../html/index.html">Temps de Ventre</a></h1>
+    <h1 id="tdvText"><a href="../html/index.html">Temps de Ventre</a></h1>
 
     <div class="profile">
         <a class="profile-link" href="../php/profile.php">
@@ -46,92 +113,81 @@
             </ul>
         </nav>
     </header>
-    <style>
-        body{
-            background-color: #ffffff;
-            background-image: linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%);       
-        }
-        .payment-form .form-control {
-            width: 100%;
-            max-width: 300px; 
-            margin: 5px auto;
-        }
-
-        h3{
-            text-align: center;
-        }
-    </style>
 
     <h2>Cart Details</h2>
 
-    <?php
-    include("../php/dataconnection.php");
+    <div class="container">
+        <?php
+        include("../php/dataconnection.php");
 
-    // Query item from cart
-    $query = "SELECT c.cart_id, p.product_name, c.quantity, p.price 
-              FROM cart AS c
-              INNER JOIN product AS p ON c.product_id = p.product_id
-              WHERE c.customer_id = '{$_SESSION['customer_id']}'";
-    $result = $connection->query($query);
+        // Query item from cart
+        $query = "SELECT c.cart_id, p.product_name, c.quantity, p.price 
+                FROM cart AS c
+                INNER JOIN product AS p ON c.product_id = p.product_id
+                WHERE c.customer_id = '{$_SESSION['customer_id']}'";
+        $result = $connection->query($query);
 
-    if ($result->num_rows > 0) {
-        // Display the table header
-        echo "
-        <table>
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th>Subtotal</th>
-                </tr>
-            </thead>
-            <tbody>
-        ";
-
-        // Initialize total price
-        $totalPrice = 0;
-
-        // Iterate over cart items
-        while ($row = $result->fetch_assoc()) {
-            $cartID = $row['cart_id'];
-            $productName = $row['product_name'];
-            $quantity = $row['quantity'];
-            $price = $row['price'];
-            $subtotal = $quantity * $price;
-            $totalPrice += $subtotal;
-
-            // Display the cart item row
+        if ($result->num_rows > 0) {
+            // Display the table header
             echo "
-            <tr>
-                <td>$productName</td>
-                <td>$quantity</td>
-                <td>RM$price</td>
-                <td>RM$subtotal</td>
-            </tr>
+            <div class='table-responsive'>
+                <table class='table table-bordered table-hover table-rounded'>
+                    <thead class='thead-dark'>
+                    <tr>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
             ";
+
+            // Initialize total price
+            $totalPrice = 0;
+
+            // Iterate over cart items
+            while ($row = $result->fetch_assoc()) {
+                $cartID = $row['cart_id'];
+                $productName = $row['product_name'];
+                $quantity = $row['quantity'];
+                $price = $row['price'];
+                $subtotal = $quantity * $price;
+                $totalPrice += $subtotal;
+
+                // Display the cart item row
+                echo "
+                <tr>
+                    <td>$productName</td>
+                    <td>$quantity</td>
+                    <td>RM$price</td>
+                    <td>RM$subtotal</td>
+                </tr>
+                ";
+            }
+
+            // Display the total price
+            echo "
+                <tr>
+                    <td colspan='3'>Total</td>
+                    <td>RM$totalPrice</td>
+                </tr>
+            ";
+
+            // Close the table
+            echo "
+                </tbody>
+            </table>
+            ";
+        } else {
+            echo "<p>Cart is empty</p>";
         }
 
-        // Display the total price
-        echo "
-            <tr>
-                <td colspan='3'>Total</td>
-                <td>RM$totalPrice</td>
-            </tr>
-        ";
-
-        // Close the table
-        echo "
-            </tbody>
-        </table>
-        ";
-    } else {
-        echo "<p>Cart is empty</p>";
-    }
-
-    // Close the database connection
-    $connection->close();
+        // Close the database connection
+        $connection->close();
     ?>
+    </div>
+
 
     <h2>Payment Details</h2>
 
